af17b47a229376023e8b3e9aac1070f40f42cdbf
Roman N <mr.rnazarov@gmail.com>
2025-09-25 17:54:09 +0300

    homework_3_1 uncomment

--- lesson_3/homework_3_1.py
+++ lesson_3/homework_3_1.py
@@ -1,83 +1,83 @@
-# #1
-# print("Привет, мир!")
-# print(5, 10, 15)
+#1
+print("Привет, мир!")
+print(5, 10, 15)
 
-# print(10 + 25)
+print(10 + 25)
 
-# #2
+#2
 
-# print(1, 2, 3, sep='&')
-# a = "Python"
-# b = "лучший язык"
-# print(a, b,  end="")
+print(1, 2, 3, sep='&')
+a = "Python"
+b = "лучший язык"
+print(a, b,  end="")
 
-# #3
+#3
 
-# x = 3.14
-# y = -8
-# print(f'Координаты точки: x = {x}; y = {y}')
+x = 3.14
+y = -8
+print(f'Координаты точки: x = {x}; y = {y}')
 
-# name = input("Введите имя: ") 
-# age = input("Введите возраст: ") 
-# print(f"Имя: {name}, Возраст: {age}")
+name = input("Введите имя: ") 
+age = input("Введите возраст: ") 
+print(f"Имя: {name}, Возраст: {age}")
 
-# #4
-# a = input("Введи имя: ")
-# print(f"Привет, {a}!")
+#4
+a = input("Введи имя: ")
+print(f"Привет, {a}!")
 
-# #5
-# a = input("Введи число a: ")
-# b = input("Введи число b: ")
-# print(a + b)
+#5
+a = input("Введи число a: ")
+b = input("Введи число b: ")
+print(a + b)
 
-# c = int(input("Введицелое число: "))
-# print(f"Число в квадрате: {c ** 2}")
+c = int(input("Введицелое число: "))
+print(f"Число в квадрате: {c ** 2}")
 
-# #1
-# print(5 > 3)
-# print(10 < 2)
-# print(7 == 7) 
-# print(6 != 8)
-# print(4 >= 4)
-# print(9 <= 3)
+#1
+print(5 > 3)
+print(10 < 2)
+print(7 == 7) 
+print(6 != 8)
+print(4 >= 4)
+print(9 <= 3)
 
-# res = 8 > 12
-# print("Значение res:", res) 
-# print(type(res)) #
+res = 8 > 12
+print("Значение res:", res) 
+print(type(res)) #
 
-# #2
-# x = 15
-# print(x % 2 == 0)
-# print(x // 5) 
-# print((x % 3 == 0) and (x % 5 == 0)) 
+#2
+x = 15
+print(x % 2 == 0)
+print(x // 5) 
+print((x % 3 == 0) and (x % 5 == 0)) 
 
-# #3
-# y = 4.5
-# print(y >= 1 and y <= 10)
-# print((y >= 0 and y <= 5) or (y >=10 and y <= 15))
+#3
+y = 4.5
+print(y >= 1 and y <= 10)
+print((y >= 0 and y <= 5) or (y >=10 and y <= 15))
 
-# #4
+#4
 
-# print(True or False and False) # True
-# print(not False and True) # True
-# print(False or not True and True) ## False
-# print(not (10 > 5 or 3 < 1)) # False
+print(True or False and False) # True
+print(not False and True) # True
+print(False or not True and True) ## False
+print(not (10 > 5 or 3 < 1)) # False
 
-# #5
-# a = 200
-# print(bool(0)) # False
-# print(bool(-5)) # True
-# print(bool(3.14)) # True
-# print(bool("")) # False
-# print(bool("Python")) # True
-# print(bool(" ")) # True
+#5
+a = 200
+print(bool(0)) # False
+print(bool(-5)) # True
+print(bool(3.14)) # True
+print(bool("")) # False
+print(bool("Python")) # True
+print(bool(" ")) # True
 
-# #6
+#6
 
-# n = 4
-# print(n > 0) #True
-# print(n % 2 == 0) #True
-# print(n % 3 == 0) #False
+n = 4
+print(n > 0) #True
+print(n % 2 == 0) #True
+print(n % 3 == 0) #False
 
 
 #1
